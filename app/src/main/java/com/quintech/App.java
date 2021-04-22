2e705bc0e9262b347390b42a99625d17d8b561a5
yona-asael <yona_art@hotmail.com>
2021-04-22 15:34:14 -0500

    gradle added

---
+++ app/src/main/java/com/quintech/App.java
@@ -0,0 +1,8 @@
+package com.quintech;
+
+public class App {
+
+    public static void main(String[] args) {
+      
+    }
+}
---
+++ app/src/test/java/com/quintech/AppTest.java
@@ -0,0 +1,10 @@
+package com.quintech;
+
+import org.junit.Test;
+
+public class AppTest {
+    @Test 
+    public void testAppHasAGreeting() {
+    
+    }
+}
